71fd96e3ddc1da9085f443c6c9486b3864e658c4
CAIQUE ARAUJO SILVA <caique.asilva17@senacsp.edu.br>
2022-09-02 22:01:20 -0300

    Vetores, funções e template string

---
+++ calculaservico.js
@@ -0,0 +1,4 @@
+/*
+Construir uma matriz serviço e prazo. serviço feito em 24h, 48h, 72h. 
+Quanto mais rápido o serviço, mais caro ele fica
+*/
---
+++ vetores.js
@@ -0,0 +1,26 @@
+let vetor = ['Caique' , 'Araujo', 'Silva', 'Pereira', 'Severino',
+ 'Rosa', 'Machado', 'Arruda' ]
+
+//console.log(vetor);
+
+/*
+//looping com o vetor
+for (var i = 0; i < vetor.length; i++) {
+    console.log(`Nome: ${vetor[i]}`);
+}
+
+
+//Foreach For = para each = cada
+vetor.forEach(function(valor, indice){
+    console.log(`${indice}: ${valor}`)
+})
+*/
+
+//map
+
+/*
+vetor.map(function(val, val2){
+    console.log(val2);
+});
+*/
+
