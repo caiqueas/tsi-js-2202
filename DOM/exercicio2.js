ad9e3cf325c096d04567e78a8d7d32085be60a15
CAIQUE ARAUJO SILVA <caique.asilva17@senacsp.edu.br>
2022-09-09 22:13:58 -0300

    DOM e eventos

---
+++ DOM/exercicio2.js
@@ -0,0 +1,19 @@
+let btnCalcular = document.getElementById('calcular');
+
+btnCalcular.addEventListener('click', function (evento){
+    evento.preventDefault();
+    console.log(evento);
+
+    //Obter os valor de serviço e prazo
+
+});
+
+/*
+btnCalcular.addEventListener('click', executa())
+
+function execute(){
+    alert('Clicou');
+}
+*/
+
+//console.log(btnCalcular);
--- calculaservico.js
+++ calculaservico.js
@@ -2,3 +2,21 @@
 Construir uma matriz serviço e prazo. serviço feito em 24h, 48h, 72h. 
 Quanto mais rápido o serviço, mais caro ele fica
 */
+
+let tabela = [  ['$$$','$$', '$'],
+                ['$$$$', '$$$', '$$'],
+                ['Não faz', 'Não faz', '$$$']];
+
+let servico = prompt("Entre com o serviço :\n 0 - Troca de óleo \n 1 - Balanceamento\n 2 - Cambagem");
+let prazo = prompt("Entre com o prazo :\n 0 - Um dia \n 1 - Dois dias\n 2 - Três dias")
+
+if( validaEntrada(servico) && validaEntrada(prazo)){
+    alert("O serviço ficará em: " + tabela[servico][prazo]);
+}else{
+    alert("ERRO: Verifique os valores inseridos e tente novamente");
+}
+
+function validaEntrada(dadoUsuario){
+    return dadoUsuario >= 0 && dadoUsuario <= 2 ? true : false;
+
+}
