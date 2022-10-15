610762a7cf6abdbee9e365c28653a1872933a916
CAIQUE ARAUJO SILVA <caique.asilva17@senacsp.edu.br>
2022-10-14 22:07:45 -0300

    Criando e excluindo tarefas JS

--- STORAGE/localStorage.js
+++ STORAGE/localStorage.js
@@ -10,35 +10,24 @@ btn.addEventListener('click', mostrar);
 function mostrar(evento){
 
     evento.preventDefault();
-
         document.getElementById('entrada');  
-
         document.getElementById('mostrar');  
-
         let valor = entrada.value;
-
         mostrar.innerText = valor;
-
         entrada.value = '';
 
         //após obtermos o dado do usuário 
-
         //vamos guardá-lo no local storage
 
         localStorage.setItem('dadoDoUsuario', valor);
-
 }
 
     //Para recuperar o dado do local storage 
-
     //usamos o localStorage.getItem()
 
     document.querySelector('#mostrar').innerText = 
-
         localStorage.getItem('dadoDoUsuario');
 
     //E para apagar no local storage?
-
     //removeItem()
-
     //localStorage.removeItem('dadoDoUsuario');
--- STORAGE/sessionStorage.js
+++ STORAGE/sessionStorage.js
@@ -1,5 +1,6 @@
-localStorage.setItem('nome', 'Camila');
-localStorage.setItem('sobrenome', 'Silva');
+sessionStorage.setItem('nome', 'Camila');
+
+sessionStorage.setItem('sobrenome', 'Silva');
 
 let btn = document.getElementById('botao');
 
@@ -8,26 +9,50 @@ let btn = document.getElementById('botao');
 btn.addEventListener('click', mostrar);
 
 function mostrar(evento){
-
     evento.preventDefault();
-        document.getElementById('entrada');  
-        document.getElementById('mostrar');  
+
+        let entrada = document.getElementById('entrada');  
+        let mostrando = document.getElementById('mostrar');  
         let valor = entrada.value;
-        mostrar.innerText = valor;
+        mostrando.innerText = valor;
 
         entrada.value = '';
 
         //após obtermos o dado do usuário 
-        //vamos guardá-lo no session storage
+        //vamos guardá-lo no local storage
 
-        localStorage.setItem('dadoDoUsuario', valor);
+        sessionStorage.setItem('dadoDoUsuario', valor);
 }
-    //Para recuperar o dado do session storage 
-    //usamos o localStorage.getItem()
+    //Para recuperar o dado do local storage 
+    //usamos o sessionStorage.getItem()
 
     document.querySelector('#mostrar').innerText = 
-        localStorage.getItem('dadoDoUsuario');
+        sessionStorage.getItem('dadoDoUsuario');
 
-    //E para apagar no session storage?
+    //E para apagar no local storage?
     //removeItem()
-    //localStorage.removeItem('dadoDoUsuario');
+    //sessionStorage.removeItem('dadoDoUsuario');
+
+let vetor = ['pera ', 'banana ', 'manga '];
+
+//Para guardar  algo que não seja uma String no LocalStorage
+//ou SessionStorage, temos que converter para string.
+//Com o método "JSON.stringify()", transformamos vetores e objetos
+//em string
+
+sessionStorage.setItem('frutas', JSON.stringify(vetor));
+
+//let recuperaDado = sessionStorage.getItem('frutas');
+
+//console.log(recuperaDado[1]);
+
+let recupDado = JSON.parse(sessionStorage.getItem('frutas'));
+
+recupDado[3] ='morango';
+recupDado[1] ='parchita';
+
+let strRecupDados = JSON.stringify(recupDado);
+
+console.log(strRecupDados);
+
+sessionStorage.setItem('frutas', strRecupDados);
